5d009b65f16942e2a7a1d9b22eb7cf08fc284404
Alyssa Slayton <ajs41@missouristate.edu>
2018-02-11 19:37:49 -0600

    all tests passed 15/15, style changes made

--- csc131/lab02.py
+++ csc131/lab02.py
@@ -4,9 +4,9 @@ def fact_recursive(n: int) -> int:
     :param n: The value whose factorial we seek
     :return: n! is returned; -1 is returned if n < 0.
     """
-    if(n<0):
+    if n < 0:
         return -1
-    elif (n == 0):
+    elif n == 0:
         return 1
     else:
         return n*fact_recursive(n-1)
@@ -22,13 +22,13 @@ def fact_iterative(n: int) -> int:
     :return: n! is returned; -1 is returned if n < 0.
     """
     temp = 1
-    if (n < 0):
+    if n < 0:
         return -1
-    elif(n==0):
+    elif n == 0:
         return 1
     else:
         for index in range(1,n+1):
-            temp*=index
+            temp *= index
 
     # TODO: Implemented me correctly using iteration.
     return temp
@@ -54,9 +54,9 @@ def fib(n: int) -> int:
     :param n: The Fibonacci number to compute; n > 0
     :return: The nth Fibonacci number is returned; -1 is returned if n < 1
     """
-    if(n<1):
+    if n < 1:
         return -1
-    elif(n==1 or n==2):
+    elif n == 1 or n == 2:
         return 1
     else:
         return fib(n-1)+fib(n-2)
